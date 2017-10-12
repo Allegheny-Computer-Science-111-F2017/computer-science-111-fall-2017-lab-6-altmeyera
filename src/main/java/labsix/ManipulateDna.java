17aa0c2ef533f732e95a9b53ba144df5f35d2aaf
Aaron Altmeyer <altmeyera@aldenv180.allegheny.edu>
2017-10-12 15:21:57 -0400

    Lab 6 Complete

--- src/main/java/labsix/ManipulateDna.java
+++ src/main/java/labsix/ManipulateDna.java
@@ -3,13 +3,24 @@ package labsix;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Date;
+import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * This is lab six for Computer Science 111.
+ * This lab will teach us to use the random class.
+ * We will use this random class multiple times in this assignment.
+ */
 public class ManipulateDna {
-
+  /**
+   * This program will take a string of DNA and manipulate it.
+   * Manipulations will be either inserting, deleting, or changing a random place in the DNA.
+   * It will create multiple lines of output where we do all 3 manipulations.
+   * I use a new random integer for each manipulation.
+   */
   public static void main(String[] args) {
     // display the name of the programmer and the date
-    System.out.println("Gregory M. Kapfhammer " + new Date());
+    System.out.println("Aaron Altmeyer " + new Date());
     // declare the starting file and scanner
     File dnaInputsFile = null;
     Scanner scanner = null;
@@ -21,17 +32,41 @@ public class ManipulateDna {
       System.out.println("Unable to locate file");
     }
     // Step One: Read in and display the chosen DNA string
-
+    String dna = scanner.nextLine();
     // Step Two: Compute the complement of the DNA String
+    String udna = dna.toUpperCase();
+    System.out.println("Okay, I am going to manipulate the DNA string: " + udna);
+    System.out.println(" ");
+    System.out.println("Enter a string containing only C, G, T, and A: " + udna);
+    String comp1 = dna.replace('a', 'T');
+    String comp2 = comp1.replace('t', 'A');
+    String comp3 = comp2.replace('c', 'G');
+    String comp4 = comp3.replace('g', 'C');
+    System.out.println("Complement of " + udna + " is " + comp4);
 
     // Step Three: Insert a randomly chosen DNA letter into the DNA String
+    Random rnd = new Random();
+    int inspos = rnd.nextInt(udna.length());
+    String dnaletters = "ATCG";
+    char letter = dnaletters.charAt(rnd.nextInt(dnaletters.length()));
+    String insertdna = udna.substring(0, inspos) + letter + udna.substring(inspos);
+    System.out.println("Inserting from positon " + inspos + " gives: " + insertdna);
 
     // Step Four: Delete a DNA letter from a randomly chosen position in the DNA string
+    int delpos = rnd.nextInt(udna.length());
+    String deletedna = udna.substring(0, delpos) + udna.substring(delpos + 1);
+    System.out.println("Deleting from position " + delpos + " gives: " + deletedna);
 
     // Step Five: Change a random position in the DNA String to a randomly chosen DNA letter
-
+    int cpos = rnd.nextInt(udna.length());
+    String dd2 = udna.substring(0, cpos) + udna.substring(cpos + 1);
+    String id2 = dd2.substring(0, cpos) + letter + dd2.substring(cpos);
+    System.out.println("Changing from position " + cpos + " gives: " + id2);
+    
     // Step Six: Display a final thankyou message
-
+    System.out.println(" ");
+    System.out.println("Thanks for using the ManipulateDna program.");
+    System.out.println("Have an awesome day.");
   }
 
 }
--- writing/reflection.md
+++ writing/reflection.md
@@ -1,4 +1,16 @@
-# Reflection by
+# Reflection by Aaron Altmeyer
 
-This lab required me to write a file called `ManipulateDNA`. Please review the
-Laboratory Assignment Sheet and add the required details to this paragraph.
+This lab required me to write a file called `ManipulateDNA`.  This file created
+multiple lines of output that did various manipulations: insert, delete and
+change.  In order to do this, I used the random class to create random integers
+and random characters for these manipulations.  This program is interesting and
+challenging at the same time.  I haven't struggled this much with a lab.  I
+think the most difficult aspect of creating this program is understanding what
+it is you need to code.  Replacing letters was simple, but when we started
+using substrings it got a bit confusing.  Once I figured out how to properly do
+the substrings for my deletions and insertions it became rather simple.  I
+finished step five quickly after struggling with steps three and four.
+
+Overall, I think this is the most interesting program that we have made, partly
+because I am a biology major and I have heard about bioinformatics before.  It
+is definitely a course that I would be interested in taking in the future.
